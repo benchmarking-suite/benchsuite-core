dfc4c83acc7f55a471d2718949f60401a265a0b4
Gabriele Giammatteo <gabriele.giammatteo@eng.it>
2017-07-05 16:25:38 +0200

    fixed packages

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name='benchsuite.core',
-    version='2.0.0-dev3',
-    packages=['benchsuite.core'],
+    version='2.0.0-dev12',
+    packages=find_packages('src'),
+    namespace_packages=['benchsuite'],
     package_dir={'': 'src'},
     url='https://github.com/benchmarking-suite/benchmarking-common',
     license='',
